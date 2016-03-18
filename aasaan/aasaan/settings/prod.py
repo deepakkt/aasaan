cffad8c8028433e87aca0a831d62e74fd95491b0
Deepak <deepak.kumaran@ishafoundation.org>
2016-03-18 18:28:54 +0530

    Changes for deployment. Most changes in prod.py

--- aasaan/aasaan/settings/prod.py
+++ aasaan/aasaan/settings/prod.py
@@ -1,7 +1,21 @@
 from .base import *
 
+# for prod environments, the static items are served with
+# Nginx from a hardcoded path instead of relative paths
+# like in dev environments. So override the paths
+# present in base.py
+del STATIC_PATH
+STATIC_ROOT = "/var/www/static"
+MEDIA_ROOT = "/var/www/media"
+STATICFILES_DIRS = (STATIC_ROOT,)
+
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-#to be filled in during deployment
+# to be filled in during deployment
 ALLOWED_HOSTS = []
+
+# deploy on gunicorn on linux boxes
+INSTALLED_APPS += (
+    'gunicorn',
+)
