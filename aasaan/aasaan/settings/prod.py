790ada4ba20e5980d268054b62ec07a24c7d4324
Deepak <deepak.kumaran@ishafoundation.org>
2016-04-07 19:26:42 +0530

    For deploy on amazon Since it will be hosting three sites keep static under /var/www/aasaan/* for this project

--- aasaan/aasaan/settings/prod.py
+++ aasaan/aasaan/settings/prod.py
@@ -5,15 +5,17 @@ from .base import *
 # like in dev environments. So override the paths
 # present in base.py
 del STATIC_PATH
-STATIC_ROOT = "/var/www/static"
-MEDIA_ROOT = "/var/www/media"
+STATIC_ROOT = "/var/www/aasaan/static"
+MEDIA_ROOT = "/var/www/aasaan/media"
 # STATICFILES_DIRS = (STATIC_ROOT,)
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 # to be filled in during deployment
-ALLOWED_HOSTS = ["188.166.245.115", "aasaan.isha.in"]
+ALLOWED_HOSTS = ["188.166.245.115",
+                 "52.72.197.233",
+                 "aasaan.isha.in"]
 
 # deploy on gunicorn on linux boxes
 INSTALLED_APPS += (
