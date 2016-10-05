b75f15b15d5be9d55f1f1afac6f1d667c5ede9ca
Manimaran K <manimaran.k@ishafoundation.org>
2016-10-05 12:47:10 +0530

    attachments name change

--- aasaan/ipctravels/admin.py
+++ aasaan/ipctravels/admin.py
@@ -2,26 +2,32 @@ from django.contrib import admin
 from .models import TravelRequest, AgentMaster, TravelModeMaster, BudgetCodeMaster, BookingDetails, TravellerDetails, \
     TicketDetails, AddtionalDetails
 from daterange_filter.filter import DateRangeFilter
-
+from django.utils.text import slugify
 
 class BookingDetailsInline(admin.StackedInline):
     model = BookingDetails
     extra = 1
     max_num = 5
-
     fieldsets = (
         ('', {
             'fields': (('date_of_journey', 'date_of_booking'), ('source', 'destination'),
-                       ('travel_mode', 'description'), ('booked_by', 'budget_code')),
+                       ('travel_mode', 'description'), ('booked_by')),
             'classes': ('has-cols', 'cols-2')
         }),
     )
 
 
-class TravellerDetailsInline(admin.TabularInline):
+class TravellerDetailsInline(admin.StackedInline):
     model = TravellerDetails
     extra = 1
-
+    max_num = 5
+    fieldsets = (
+        ('', {
+            'fields': (('traveller', 'non_ipc_contacts'), ('schedule', 'purpose'),
+                       ('zone', 'status'), ('fare', 'refund_amount'), ('budget_code')),
+            'classes': ('has-cols', 'cols-2')
+        }),
+    )
 
 class TicketDetailsInline(admin.TabularInline):
     model = TicketDetails
@@ -38,15 +44,15 @@ class TravelRequestAdmin(admin.ModelAdmin):
     fieldsets = (
         ('',
          {
-             'fields': (('status', 'total_fare'), ('request_mode', 'cancellation_charge')),
-             'classes': ('has-cols', 'cols-2')
+             'fields': (('status', 'request_mode', 'request_date'), ('total_fare', 'refund_amount', 'cancellation_charge')),
+             'classes': ('has-cols', 'cols-3')
          }),
     )
     list_display = ('traveller', 'travel_details', 'get_status',)
     inlines = [BookingDetailsInline, TravellerDetailsInline, TicketDetailsInline, AddtionalDetailsInline]
     save_on_top = True
     list_filter = ('status', ('bookingdetails__date_of_booking', DateRangeFilter),
-                   ('bookingdetails__date_of_journey', DateRangeFilter), 'bookingdetails__booked_by',
+                   ('bookingdetails__date_of_journey', DateRangeFilter), ('request_date', DateRangeFilter), 'bookingdetails__booked_by',
                    'bookingdetails__travel_mode')
 
 admin.site.register(AgentMaster, admin.ModelAdmin)
--- aasaan/ipctravels/models.py
+++ aasaan/ipctravels/models.py
@@ -1,28 +1,31 @@
 from django.db import models
-import datetime
+import datetime, time
 from contacts.models import Zone, Contact
 from schedulemaster.models import ProgramSchedule
+import os, os.path
+from django.utils.text import slugify
+from django.utils import timezone
 
 
 class AgentMaster(models.Model):
     name = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return "%s" % (self.name)
+        return "%s" % self.name
 
 
 class TravelModeMaster(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return "%s" % (self.name)
+        return "%s" % self.name
 
 
 class BudgetCodeMaster(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return "%s" % (self.name)
+        return "%s" % self.name
 
 
 class TravelRequest(models.Model):
@@ -34,9 +37,10 @@ class TravelRequest(models.Model):
                      ('NP', 'Not processed'),)
     status = models.CharField(max_length=6, choices=STATUS_VALUES, blank=False,
                               default=STATUS_VALUES[0][0])
-    request_date = models.DateTimeField(default=datetime.datetime.now(), )
+    request_date = models.DateField(default=timezone.now)
     total_fare = models.IntegerField(blank=True, null=True)
     cancellation_charge = models.IntegerField(blank=True, null=True)
+    refund_amount = models.IntegerField(blank=True, null=True)
     REQMODE_VALUES = (('SMS', 'SMS'),
                      ('CL', 'Call'),
                      ('EM', 'Email'),
@@ -76,7 +80,6 @@ class BookingDetails(models.Model):
     date_of_booking = models.DateField(blank=True, null=True)
     travel_mode = models.ForeignKey(TravelModeMaster, default=1)
     booked_by = models.ForeignKey(AgentMaster, default=1)
-    budget_code = models.ForeignKey(BudgetCodeMaster, default=1)
 
     def __str__(self):
         return "%s" % self.date_of_journey
@@ -85,25 +88,33 @@ class BookingDetails(models.Model):
 class TravellerDetails(models.Model):
     request = models.ForeignKey(TravelRequest)
     traveller = models.ForeignKey(Contact)
+    non_ipc_contacts = models.CharField(max_length=100, blank=True, null=True)
     fare = models.IntegerField(blank=True, null=True)
     refund_amount = models.IntegerField(blank=True, null=True)
     zone = models.ForeignKey(Zone, blank=True, null=True)
     schedule = models.ForeignKey(ProgramSchedule, blank=True, null=True)
+    purpose = models.CharField(max_length=100, blank=True, null=True)
     STATUS_VALUES = (('BK', 'Booked'),
                      ('NB', 'Not booked'),
                      ('CD', 'Cancelled'),
                      )
     status = models.CharField(max_length=6, choices=STATUS_VALUES, blank=False,
                               default=STATUS_VALUES[0][0])
+    budget_code = models.ForeignKey(BudgetCodeMaster, default=1)
+
+
+def _generate_file_path(instance, filename):
+    file_extension = os.path.splitext(filename)[-1]
+    travel_ticket_name = slugify(time.strftime(os.path.splitext(filename)[0] +'_'+ "%Y%m%d%H%M%S")) + file_extension
+    return os.path.join('travel_tickets', travel_ticket_name)
 
 
 class TicketDetails(models.Model):
     request = models.ForeignKey(TravelRequest)
-    attachment = models.FileField(blank=True)
+    attachment = models.FileField(upload_to=_generate_file_path, blank=True)
 
 
 class AddtionalDetails(models.Model):
     request = models.ForeignKey(TravelRequest)
-
     booked_by = models.CharField("booked by", max_length=50)
     purpose = models.CharField("Purpose", max_length=50)
