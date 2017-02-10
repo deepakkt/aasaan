ac6ca15203c2aec8fc00f572e1ca55da6d7c8aa8
RaghavendraC <user@iyc4pclt5.iyc.ishafoundation.org>
2017-02-10 09:29:48 +0530

    Removed logs and Formatted the code

--- aasaan/static/misc/js/synch_sheets.js
+++ aasaan/static/misc/js/synch_sheets.js
@@ -1,41 +1,37 @@
-var URL="http://aasaan.isha.in/config/admin_dashboard";
-var CMD='';
-var SPAN_ID='';
+var URL = "http://aasaan.isha.in/config/admin_dashboard";
+var CMD = '';
+var SPAN_ID = '';
 var MSG = 'MESSAGE IS NOT FROM THE SERVER';
-function setCommand(txtcmd,txtSpanId) { 
+
+function setCommand(txtcmd, txtSpanId) {
     CMD = txtcmd;
-	SPAN_ID=txtSpanId;
-	console.log('------Start setCommand method------');
-    console.log('Command :->'+CMD+'\t SPAN ID :->'+SPAN_ID);
-    console.log('------End   setCommand method------');
-	return true;
+    SPAN_ID = txtSpanId;    
+    return true;
 }
 
-$(document).ready(function(){
-   for(var i=4;i<=12;i++){
-    $("#id_button"+i).hide();
-   }  
-   $('button[type="submit"]').on('click', function() {   		
-	    console.log('------Start jQuery on click method------');
-        console.log('Command  :->'+CMD);
-        console.log('------End   jQuery on click method------');
+$(document).ready(function() {
+    for (var i = 4; i <= 12; i++) {
+        $("#id_button" + i).hide();
+    }
+    $('button[type="submit"]').on('click', function() {        
         var result = $.ajax({
             url: URL,
-		    data:{command: CMD},
+            data: {
+                command: CMD
+            },
             type: "POST",
             dataType: "json",
             timeout: 20000,
-            async: false, 
-		}).responseText;
-        if(result.code=='success'){ 
-		   $("#"+SPAN_ID).html(result.message).addClass('success').slideUp("slow").slideDown("slow");
-		}else if(result.code=='warning'){
-		   $("#"+SPAN_ID).html(result.message).addClass('warning').slideUp("slow").slideDown("slow");
-		}else if(result.code=='error'){
-		   $("#"+SPAN_ID).html(result.message).addClass('error').slideUp("slow").slideDown("slow");
-		}
-		else{
-		   $("#"+SPAN_ID).html(MSG).slideUp("slow").slideDown("slow"); 
-		}	
+            async: false,
+        }).responseText;
+        if (result.code == 'success') {
+            $("#" + SPAN_ID).html(result.message).addClass('success').slideUp("slow").slideDown("slow");
+        } else if (result.code == 'warning') {
+            $("#" + SPAN_ID).html(result.message).addClass('warning').slideUp("slow").slideDown("slow");
+        } else if (result.code == 'error') {
+            $("#" + SPAN_ID).html(result.message).addClass('error').slideUp("slow").slideDown("slow");
+        } else {
+            $("#" + SPAN_ID).html(MSG).slideUp("slow").slideDown("slow");
+        }
     });
 });
