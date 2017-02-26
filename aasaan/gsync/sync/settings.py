208d73d6d84730c1287d22268c5db3e6a91313d6
Deepak <deepak.kumaran@ishafoundation.org>
2017-02-26 13:40:20 +0000

    extending base rows for enrollment count categories, fix only if valid identifiers are available

--- aasaan/gsync/sync/settings.py
+++ aasaan/gsync/sync/settings.py
@@ -71,7 +71,9 @@ schedule_enrollment_sync_rows = ['SNo',
                                  'ORS Code',
                                  'Teacher']
 
-schedule_enrollment_sync_rows.extend(enrollment_count_categories)
+if enrollment_count_categories:
+	schedule_enrollment_sync_rows.extend(enrollment_count_categories)
+
 schedule_enrollment_sync_rows.extend(['ID', 'Last Modified'])
 
 schedule_enrollment_header = namedtuple('ScheduleEnrollment', [slugify(x).replace("-", "_")
