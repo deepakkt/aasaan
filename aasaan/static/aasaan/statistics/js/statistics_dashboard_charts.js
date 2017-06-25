9b221668bbbec1384e179dc14c30f26eced0c45e
manimaran_k <manimaran.k@ishafoundation.org>
2017-06-25 09:19:32 +0530

    Statistics IYC chart and table not working - issue fixed

--- aasaan/static/aasaan/statistics/js/statistics_dashboard_charts.js
+++ aasaan/static/aasaan/statistics/js/statistics_dashboard_charts.js
@@ -1,35 +1,55 @@
 /**
  * Created by manimaran on 21-12-2016.
  */
-refresh_reports = null;
+refresh_charts = null;
+refresh_tables = null;
 
 (function($) {
+    isChart = true;
+    isChartClicked = true;
     google.charts.load('current', {'packages':['corechart', 'table']});
     google.charts.setOnLoadCallback(drawVisualization);
 
-    function refreshReports(r_zone) {
+    function refreshCharts(r_zone) {
         var currentZone = document.getElementById(aasaan_stats_dashboard.report_zone).innerText;
         var clickedZone = r_zone;
         var clickedZoneName = document.getElementById(r_zone).innerText;
-        if (currentZone == clickedZoneName) {
+        if (currentZone == clickedZoneName && isChartClicked) {
             // active zone was clicked. no action is required
             return
         }
+        isChartClicked = true
+        refresh(clickedZone, clickedZoneName, r_zone, true);
+    }
 
+    function refreshTables(r_zone) {
+        var currentZone = document.getElementById(aasaan_stats_dashboard.report_zone).innerText;
+        var clickedZone = r_zone;
+        var clickedZoneName = document.getElementById(r_zone).innerText;
+        if (currentZone == clickedZoneName && isChartClicked==false) {
+            // active zone was clicked. no action is required
+            return
+        }
+        isChartClicked = false
+        refresh(clickedZone, clickedZoneName, r_zone, false);
+    }
+
+    function refresh(clickedZone, clickedZoneName, r_zone, isChart){
         document.getElementById(clickedZone).parentNode.className = "active";
         document.getElementById(aasaan_stats_dashboard.report_zone).parentNode.className = "";
         aasaan_stats_dashboard.report_zone = r_zone;
-
         pageHeader = document.getElementsByClassName("page-header")[0];
         pageHeader.innerText = "Statistics Dashboard - " + clickedZoneName;
-
-        drawVisualization(clickedZoneName);
+        drawVisualization(clickedZoneName, isChart);
     }
-    refresh_reports = refreshReports;
-    function drawVisualization(zone) {
+    refresh_charts = refreshCharts;
+    refresh_tables = refreshTables;
+    function drawVisualization(zone, isChart) {
 
         if(zone==undefined || zone=='undefined')
             zone='Tamil Nadu'
+        if(isChart==undefined || isChart=='undefined')
+            isChart=true
 
         var options = {
           title : '',
@@ -50,41 +70,21 @@ refresh_reports = null;
             ie_data = google.visualization.arrayToDataTable(stats_data.TN_IE)
             other_prg_data = google.visualization.arrayToDataTable(stats_data.TN_OTHER)
             program_avg = google.visualization.arrayToDataTable(stats_data.TN_AVG)
-            $("#row-tn-other-table").show()
-            $("#row-tn-ie-table").show()
-            $("#row-tn-other-chart").show()
-            $("#row-tn-ie-chart").show()
-            $("#row-tn-class-average").show()
         }
         else if (zone=='Outside Tamilnadu'){
             ie_data = google.visualization.arrayToDataTable(stats_data.OTN_IE_PROGRAMS)
             other_prg_data = google.visualization.arrayToDataTable(stats_data.OTN_OTHER_PROGRAMS)
             program_avg = google.visualization.arrayToDataTable(stats_data.OTN_AVG)
-            $("#row-tn-other-table").show()
-            $("#row-tn-ie-table").show()
-            $("#row-tn-other-chart").show()
-            $("#row-tn-ie-chart").show()
-            $("#row-tn-class-average").show()
         }
          else if (zone=='Isha Yoga Center'){
             ie_data = google.visualization.arrayToDataTable(stats_data.IYC_IE_PROGRAMS)
             other_prg_data = google.visualization.arrayToDataTable(stats_data.IYC_OTHER_PROGRAMS)
             program_avg = google.visualization.arrayToDataTable(stats_data.IYC_AVG)
-            $("#row-tn-other-table").show()
-            $("#row-tn-ie-table").show()
-            $("#row-tn-other-chart").show()
-            $("#row-tn-ie-chart").show()
-            $("#row-tn-class-average").show()
         }
         else if (zone=='Overseas'){
             ie_data = google.visualization.arrayToDataTable(stats_data.OVS_IE_PROGRAMS)
             other_prg_data = google.visualization.arrayToDataTable(stats_data.OVS_OTHER_PROGRAMS)
             program_avg = google.visualization.arrayToDataTable(stats_data.OVS_AVG)
-            $("#row-tn-other-table").show()
-            $("#row-tn-ie-table").show()
-            $("#row-tn-other-chart").show()
-            $("#row-tn-ie-chart").show()
-            $("#row-tn-class-average").show()
         }
         else if (zone=='Training'){
 //            ie_data = google.visualization.arrayToDataTable(stats_data.TRAINING)
@@ -98,13 +98,52 @@ refresh_reports = null;
         }
         else if (zone=='Uyir Nokkam'){
             ie_data = google.visualization.arrayToDataTable(stats_data.TN_UN)
-            other_prg_data = google.visualization.arrayToDataTable(stats_data.TN_UN_AVG)
-            $("#row-tn-other-table").hide()
-            $("#row-tn-ie-table").hide()
-            $("#row-tn-other-chart").hide()
-            $("#row-tn-ie-chart").show()
-            $("#row-tn-class-average").show()
+            program_avg = google.visualization.arrayToDataTable(stats_data.TN_UN_AVG)
+            $("#header_ie_chart").text('Number of participants')
+            $("#header_ie_table").text('Number of participants')
+            $("#header_other_table").text('No of classes')
+            $("#header_other_chart").text('No of classes')
+            if(isChart){
+                $("#row-tn-other-table").hide()
+                $("#row-tn-ie-table").hide()
+                $("#row-tn-other-chart").hide()
+                $("#row-tn-ie-chart").show()
+                $("#row-tn-class-average").show()
+                $("#container2").hide()
+            }
+            else{
+                $("#row-tn-other-table").show()
+                $("#row-tn-ie-table").show()
+                $("#row-tn-other-chart").hide()
+                $("#row-tn-ie-chart").hide()
+                $("#row-tn-class-average").hide()
+                $("#container2").show()
+                $("#container4").hide()
+            }
         }
+        if (zone=='Tamil Nadu' || zone=='Outside Tamilnadu' || zone=='Overseas' || zone=='Isha Yoga Center'){
+            $("#header_ie_chart").text('Number of participants - Inner Engineering & Isha Yoga Program')
+            $("#header_ie_table").text('Number of participants - Inner Engineering & Isha Yoga Program')
+            $("#header_other_table").text('Number of participants - Other programs')
+            $("#header_other_chart").text('Number of participants - Other programs')
+            $("#container2").show()
+            if(isChart){
+                $("#row-tn-other-table").hide()
+                $("#row-tn-ie-table").hide()
+                $("#row-tn-other-chart").show()
+                $("#row-tn-ie-chart").show()
+                $("#row-tn-class-average").show()
+                $("#container4").show()
+                }
+            else{
+                $("#row-tn-other-table").show()
+                $("#row-tn-ie-table").show()
+                $("#row-tn-other-chart").hide()
+                $("#row-tn-ie-chart").hide()
+                $("#row-tn-class-average").hide()
+                $("#container4").hide()
+            }
+         }
 
         var chart = new google.visualization.ComboChart(document.getElementById('tn-ie-chart'));
         chart.draw(ie_data, options);
@@ -135,14 +174,17 @@ refresh_reports = null;
                         x1: '0%', y1: '0%', x2: '100%', y2: '100%',
                         useObjectBoundingBoxUnits: true
                     }
-                }
+                },
+                alwaysOutside: true
             },
             selectionMode: 'multiple',
             tooltip: {trigger: 'selection'},
         };
 
         var chart = new google.visualization.ComboChart(document.getElementById('tn-class-average'));
-        chart.draw(program_avg, class_average_options);
+        chart.draw(program_avg, options);
+        var table = new google.visualization.Table(document.getElementById('tn-other-table'));
+        table.draw(program_avg, {showRowNumber: true, width: '100%', height: '100%'});
 
     }
 })(jQuery);
