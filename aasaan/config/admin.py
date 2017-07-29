d3674e5613ab780846b75ef04def1e1bf312645a
Deepak <deepak.kumaran@ishafoundation.org>
2017-07-29 11:26:16 +0530

    add a search field to config admin

--- aasaan/config/admin.py
+++ aasaan/config/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Configuration, Tag
 
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = search_fields = ('configuration_key',)
+
+
 # Register your models here.
-admin.site.register(Configuration)
+admin.site.register(Configuration, ConfigAdmin)
 admin.site.register(Tag)
