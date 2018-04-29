f1f76ade0f63228457f04d7d7098f5d5b17d8815
manimaran <manimaran.k@ishafoundation.org>
2018-04-29 12:41:16 +0530

    URL issue fixed aasaan.

--- aasaan/aasaan/urls.py
+++ aasaan/aasaan/urls.py
@@ -9,12 +9,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from api.aasaan_api import ScheduleResource
 from tastypie.api import Api
-
+from django.shortcuts import redirect
 
 aasaan_v1_api = Api(api_name='v1')
 aasaan_v1_api.register(ScheduleResource())
 
 urlpatterns = [
+    path('', lambda _: redirect('admin:index'), name="index"),
     path('admin/', admin.site.urls),
     path('aasaan_api/', include(aasaan_v1_api.urls)),
     path('contacts/', include(contacts.urls, namespace='contacts'), name='contacts'),
